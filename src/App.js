95bdf259cae5a812f9b6487a65a0ee33918db42b
kimiakarbin <kimiakarbin@gmail.com>
2021-09-14 10:55:40 +0430

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+function App() {
+  return (
+    <div className="App">
+      <label for="name"> Name : </label>
+      <br/>
+      <input type="text" />
+      <br/>
+      <br/>
+
+
+      <label for="lastname"> LastName : </label>
+      <br/>
+      <input type="text" />
+      <br/>
+      <br/>
+
+
+      <label for="phonenumber"> PhoneNumber : </label>
+      <br/>
+      <input type="phonenumber" />
+      <br/>
+      <br/>
+
+
+     <button>submit</button>
+
+    </div>
+  );
+}
+
+export default App;
